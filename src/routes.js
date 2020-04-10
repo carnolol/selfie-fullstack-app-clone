6a21afb74eed2c5dd7501fb2cc4ede7c751f2572
Mike Chadwick <Harakili@gmail.com>
2020-04-10 16:12:27 -0600

    .

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,10 @@
 import React from 'react'
 import {Switch, Route} from 'react-router-dom'
 import Dashboard from './components/Dashboard'
+import Form from './components/Form'
+import Product from './components/Product'
+
+export default 
+    <Switch>
+        
+    </Switch>
